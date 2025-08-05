8bd4884798b90081bfa61a3f2150d4c5a648efa3
sanketkatrajkar <135363208+sanketkatrajkar@users.noreply.github.com>
2025-08-06 02:27:41 +0530

    BST Done

--- C/BinarySearchTree.c
+++ C/BinarySearchTree.c
@@ -33,14 +33,138 @@ void Insert(PPNODE first , int no)
 
         temp = *first;
 
-        
+       while (1)
+       {
+           if(no == temp -> data)
+           {
+              printf("Dublicate data Inserted : unable to insert the data \n");
+              free(newn);
+              break;
+           }
+           else if(no > temp -> data)
+           {
+             if( temp -> rchild == NULL)
+             {
+                temp -> rchild = newn;
+                break;
+             }
+             temp = temp -> rchild;
+           }
+           else if(no < temp -> data)
+           {
+             if(temp -> lchild == NULL)
+             {
+                temp -> lchild = newn;
+                break;
+             }
+             temp = temp -> lchild;
+           }
+       }
+    }
+}
 
+void Inorder(PNODE first)
+{
+    if(first != NULL)
+    {
+        printf("%d\t",first->data);
+        Inorder(first->lchild);
+        Inorder(first->rchild);
+    }
+}
+
+void Preorder(PNODE first)
+{
+    if(first != NULL)
+    {
+        Preorder(first->lchild);
+        printf("%d\t",first->data);
+        Preorder(first->rchild);
+    }
+}
+
+void Postorder(PNODE first)
+{
+    if(first != NULL)
+    {
+        printf("%d\t",first->data);
+        Postorder(first->lchild);
+        Postorder(first->rchild);
+    }
+}
+
+bool Search(PNODE first , int no)
+{
+    bool bFlag = false;
+
+    while (first != NULL)
+    {
+        if(no == first->data)
+        {
+            bFlag = true;
+            break;
+        }
+        else if(no > first -> rchild)
+        {
+            first = first ->rchild;
+        }
+        else if(no < first -> lchild)
+        {
+            first = first -> lchild;
+        }
+
+        return bFlag;
+    } 
+}
+
+int Count(PNODE first)
+{
+    static int iCount = 0;
+
+    if(first != NULL)
+    {
+        iCount++;
+        Count(first ->lchild);
+        Count(first->rchild);
     }
 
+    return iCount; 
 }
 
 
 int main()
 {
+    PNODE head = NULL;
+    int iRet = 0;
+    bool bRet = false;
+
+    Insert(&head,30);
+    Insert(&head,20);
+    Insert(&head,40);
+
+    printf("Preorder Display :\n");
+    Preorder(head);
+
+    // printf("Inorder Display :\n");
+    // Inorder(head);
+
+    // printf("Postorder Display :\n");
+    // Postorder(head);
+
+    printf("\n");
+    bRet = Search(head,30);
+    if(bRet == true)
+    {
+        printf("Element is present\n");
+    }
+    else
+    {
+        printf("Element is not present\n");
+    }
+
+    iRet = Count(head);
+
+    printf("Number of element in BST is : %d" , iRet);
+
     return 0;
 }
---
+++ C/LinearSeach.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+void Accept(int *Arr, int iSize)
+{
+    int i = 0;
+    printf("Enter the element of array :\n");
+
+    for(i = 0; i < iSize; i++)
+    {
+        scanf("%d ",&Arr[i]);
+    } 
+}
+
+void Display(int *Arr, int iSize)
+{
+    int i = 0;
+    printf("Element of Array are :\n");
+
+    for(i = 0; i < iSize; i++)
+    {
+        printf("%d \t",Arr[i]);
+    } 
+    printf("\n");
+}
+
+bool LinearSearch(int *Arr, int iSize, int no)
+{
+    int i = 0;
+    bool bFlag = false;
+
+    for(i = 0; i < iSize; i++)
+    {
+        if(Arr[i] == no)
+        {
+            bFlag = true;
+            break;
+        }
+    }
+
+    return bFlag;
+}
+
+int main()
+{
+    int *Arr = NULL;
+    int iSize = 0;
+    bool bRet = false;
+
+    printf("Enter the size of array :\n");
+    scanf("%d",&iSize);
+
+    Arr = (int *)malloc(iSize * sizeof(int));
+
+    Accept(Arr,iSize);
+       
+    Display(Arr, iSize);
+
+    bRet = LinearSearch(Arr,iSize,5);
+    if(bRet == true)
+    {
+        printf("Element is Present\n");
+    }
+    else
+    {
+        printf("Element not Present\n");
+    }
+
+    free(Arr);
+    
+    return 0;
+}
